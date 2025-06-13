1a3eab150bfa62bc172d2d10bae74853321f54d1
NeoMonserrat <147547526+NeoMonserrat@users.noreply.github.com>
2025-06-14 03:52:11 +0800

    Added feature to create 10 processes in startup

--- grp_homework_1.cpp
+++ grp_homework_1.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <string>
 #include <thread>
@@ -14,6 +15,44 @@ int main()
         std::cout <<"Enter a command: ";
         std::getline(std::cin, command);
         processCommand(command);
+    }
+
+    stopScheduler();
+    return 0;
+}
+*/
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+#include "menu_processor.h"
+#include "scheduler.h"
+
+int main()
+{
+    printHeader();
+    startScheduler();
+
+    for (int i = 1; i <= 10; ++i)
+    {
+        addNewProcess("Process_" + std::to_string(i));
+    }
+
+    std::string command;
+
+    while (true)
+    {
+        std::cout << "Enter a command: ";
+        std::getline(std::cin, command);
+
+        processCommand(command);
+
+        if (command == "exit")
+        {
+            break;
+        }
+
     }
 
     stopScheduler();
